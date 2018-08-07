72358f012c7894eb6ce9875fa655052f65f40cf5
AJ87 <ultimateaj87@gmail.com>
2018-08-07 14:36:18 -0500

    Changes to add emails into table and to limit registration

---
+++ addUserToDB.js
@@ -0,0 +1,49 @@
+'use strict';
+var database = require('./lib/database.js');
+var logger = require('./lib/logger.js');
+var csv = require('csvtojson');
+
+let appName = 'AddUserToDB';
+
+// set up logger
+logger.initialise(appName);
+var log = logger.get();
+
+// server
+//let filePath = '/aul/fantasy/';
+// local path
+let filePath = '/Users/AJ87/Desktop/AULUserList.csv';
+
+let table = 'email_table';
+
+// initialise database
+database.initialise(log)
+.then(
+  function fullfilled(result) {
+
+    csv()
+    .fromFile(filePath)
+    .then((jsonObj)=>{
+      // add emails to DB
+        log.info(jsonObj);
+        jsonObj.map(x => updateDB(x));
+    });
+
+  },
+  function rejected(reason) {
+    log.error(`Error initialising database: ${reason}`);
+  }
+);
+
+function updateDB(obj) {
+  database.createRecord(obj, obj, table)
+  .then(
+    function fullfilled(result) {
+      // do nothing success
+    },
+    function rejected(reason) {
+      // provide error - may be ok though
+      log.info(`Couldn't update DB: ${reason}`);
+    }
+  );
+}
